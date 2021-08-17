62e5e89fe0742160984c0db927b11771a7d8470a
dhtdht020 <dhtdht020@gmail.com>
2021-08-17 18:55:51 +0300

    Added sentry

--- oscweb.py
+++ oscweb.py
@@ -1,6 +1,8 @@
 import json
 
+import sentry_sdk
 from flask import Flask, render_template, request, abort
+from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.urls import url_encode
 import subprocess
 import config
@@ -8,6 +10,17 @@ import pathlib
 
 import osc
 import utils
+
+if config.sentry_dsn:
+    sentry_sdk.init(
+        dsn=config.sentry_dsn,
+        integrations=[FlaskIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
 
 OpenShopChannel = osc.API()
 OpenShopChannel.load_packages()
