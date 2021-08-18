e30c66ff8e1437c9e3ae555f1fbb1925b930d9e6
dhtdht020 <dhtdht020@gmail.com>
2021-08-18 18:53:54 +0300

    Added sentry sample rate to config

--- oscweb.py
+++ oscweb.py
@@ -19,7 +19,7 @@ if config.sentry_dsn:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=1.0
+        traces_sample_rate=config.sample_rate
     )
 
 OpenShopChannel = osc.API()
